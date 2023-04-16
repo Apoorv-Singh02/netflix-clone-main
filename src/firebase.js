1c8122f9099f8a0fe28630af2c0315767d7b31b0
Apoorv Singh <116475419+Apoorv-Singh02@users.noreply.github.com>
2023-04-16 14:47:44 +0530

    Netflix clone with firebase hosting

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC2K5LEI0ScTsFcDjaOnMh5HofJb6LzaCE",
+    authDomain: "netflix-clone-e9fdf.firebaseapp.com",
+    projectId: "netflix-clone-e9fdf",
+    storageBucket: "netflix-clone-e9fdf.appspot.com",
+    messagingSenderId: "107455183647",
+    appId: "1:107455183647:web:251485eaaead7f3c740e2a",
+    measurementId: "G-J8RSCL0CFG"
+  };
+
+  
